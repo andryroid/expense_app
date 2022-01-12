8d9fc56142aa3829fa54155438d07695410a4468
RATSIMISETA Andry Lalaina <ratsimisetata@gmail.com>
2022-01-12 21:17:26 +0300

    control input add expense

--- lib/widgets/expense/add_expense.dart
+++ lib/widgets/expense/add_expense.dart
@@ -13,6 +13,7 @@ class AddExpense extends StatefulWidget {
 class _AddExpenseState extends State<AddExpense> {
   final titleController = TextEditingController();
   var _pickedDate = null;
+  String _errors = "";
 
   final descriptionController = TextEditingController();
 
@@ -36,40 +37,63 @@ class _AddExpenseState extends State<AddExpense> {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(25),
-      child: Column(children: [
-        TextField(
-          controller: titleController,
-          decoration: const InputDecoration(
-              labelText: "Title", icon: Icon(Icons.title)),
-        ),
-        TextField(
-          controller: descriptionController,
-          autocorrect: true,
-          decoration: const InputDecoration(
-              labelText: "Description", icon: Icon(Icons.description)),
-        ),
-        TextField(
-            controller: amountController,
-            decoration: const InputDecoration(
-                labelText: "Amount", icon: Icon(Icons.price_check_rounded)),
-            keyboardType: TextInputType.number),
-        Container(
-          height: 70,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Icon(Icons.calendar_today),
-              Text(_pickedDate == null
-                  ? 'Please pick date'
-                  : DateFormat.yMMMMd().format(_pickedDate)),
-              TextButton(
-                  onPressed: () => _presentDatePicker(),
-                  child: const Text("Choose date"))
-            ],
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              "$_errors",
+              style: const TextStyle(
+                  fontWeight: FontWeight.w500, color: Colors.red),
+            ),
           ),
-        ),
-        FloatingActionButton(
+          TextField(
+            controller: titleController,
+            decoration: const InputDecoration(
+                labelText: "Title", icon: Icon(Icons.title)),
+          ),
+          TextField(
+            controller: descriptionController,
+            autocorrect: true,
+            decoration: const InputDecoration(
+                labelText: "Description", icon: Icon(Icons.description)),
+          ),
+          TextField(
+              controller: amountController,
+              decoration: const InputDecoration(
+                  labelText: "Amount", icon: Icon(Icons.price_check_rounded)),
+              keyboardType: TextInputType.number),
+          Container(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(Icons.calendar_today),
+                Text(_pickedDate == null
+                    ? 'Please pick date'
+                    : DateFormat.yMMMMd().format(_pickedDate)),
+                TextButton(
+                    onPressed: () => _presentDatePicker(),
+                    child: const Text("Choose date"))
+              ],
+            ),
+          ),
+          FloatingActionButton(
             onPressed: () {
+              //check controller
+              String attributError = "";
+              if (titleController.text == "") attributError = "Title";
+              if (descriptionController.text == "")
+                attributError = "Description";
+              if (amountController.text == "") attributError = "Amount";
+              if (_pickedDate == null) attributError = "Date picked";
+
+              if (attributError != "") {
+                setState(() {
+                  _errors = "$attributError cannot be empty!";
+                });
+                return;
+              }
               //add expense
               widget.handleAddExpense(
                   titleController.text,
@@ -82,8 +106,13 @@ class _AddExpenseState extends State<AddExpense> {
                   applicationLegalese: "",
                   applicationIcon: const Icon(Icons.done));
             },
-            child: const Icon(Icons.add))
-      ]),
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(
+            height: 5,
+          )
+        ],
+      ),
     );
   }
 }
